eccbf4e2b93599dc30bda7e6ba5e4479859c632b
Pallavi Raman <pallaviraman@ufl.edu>
2017-09-25 11:42:56 -0400

    get and delete API for documents with specific id.

--- backend/api/routes.js
+++ backend/api/routes.js
@@ -37,6 +37,24 @@ router.get("/get", function (req, res) {
     });
 });
 
+// GET API to get entries with a specific id from database
+router.get("/get_id", function (req, res) {
+    console.log("Accepting GET request with specific id");
+    dbHelper.dbget_id(req.id,req, res, function (err, result) {
+	    if (err)
+            return res.status(400).send("Cannot obtain data specific to an id");
+		else
+		{
+			if (result) {
+				res.render('index','id'{
+					items : result
+				}); 
+			}
+			return res.status(200).send("Data obtained");
+		}
+    });
+});
+
 // DELETE API to delete the database
 router.post("/delete", function (req, res) {
     console.log("Accepting DELETE request");
@@ -47,6 +65,18 @@ router.post("/delete", function (req, res) {
             return res.status(200).send("Deleted");
     });
 });
+
+// DELETE API to delete the database with a specific id
+router.post("/delete_id", function (req, res) {
+    console.log("Accepting DELETE request with a specific id");
+    dbHelper.dbdelete_id(req.id,req, res, function (err, result) {
+        if (err)
+            return res.status(400).send("Not deleted");
+        else
+            return res.status(200).send("Deleted");
+    });
+});
+
 
 // expose to other modules
 module.exports = router;
