624c4bb610dcf9eb44ed58f78c5fc36cd79130de
Dilip Kunderu <dilipkunderu@gmail.com>
2017-10-16 17:39:29 -0400

    testing-->package.json && ng test-->angular-cli.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,7 @@ export class AppComponent implements OnInit {
   testData: Array<any> = [];
   testObj: Object;
 
+
   constructor(
     iconRegistry: MatIconRegistry,
     sanitizer: DomSanitizer,
