c93cbd8770725ceefd2497898f4fd6cbbf5fbc52
Meghana Madineni <meghanamadineni@gmail.com>
2017-11-16 15:49:40 -0500

    login and signup

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component, OnInit
+}
+from '@angular/core';
+import {
+  Router
+}
+from '@angular/router';
+
+import {
+  UserService
+}
+from '../services/index';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
+  providers: [UserService]
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router,
+      private userService: UserService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  signUpSubmit(form: any) {
+      this.userService.create(form)
+          .subscribe(
+              data => {
+                  // set success message and pass true paramater to persist the message after redirecting to the login page
+                  alert('Registration successful');
+                  this.router.navigate(['/login']);
+              },
+              error => {
+                  alert(error);
+              });
   }
-
 }
