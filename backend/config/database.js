9fc701257d5bb0600cea17a404bc84f1f9a08988
Pallavi Raman <pallaviraman@ufl.edu>
2017-11-18 21:51:25 -0500

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,4 +1,16 @@
 // config/database.js
+
+
+var elasticsearch = require('elasticsearch');
+var config = require('./config.json');
+
+var elasticclient = new elasticsearch.Client({
+    hosts: [
+        config.hostip+":"+config.port
+    ]
+});
+
+module.exports = elasticclient;
 module.exports = {
 
     'url' : 'mongodb://localhost/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
